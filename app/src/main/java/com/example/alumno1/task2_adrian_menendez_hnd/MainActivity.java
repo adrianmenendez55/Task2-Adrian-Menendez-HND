92c72b5177203412f5a606c51fa9b2fd9bd36b5b
ALUMNOS <ALUMNOS>
2017-03-07 10:16:07 +0100

    Initial commit

---
+++ app/src/main/java/com/example/alumno1/task2_adrian_menendez_hnd/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.alumno1.task2_adrian_menendez_hnd;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
